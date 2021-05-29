cb6fcc3931a3129f597176aaabe6f2f44c3b89b3
Connie Nguyen <connie.nguyen@workday.com>
2021-05-29 13:31:39 -0700

    remove unnecessary files

--- examples/webpack/src/index.js
+++
@@ -1,8 +0,0 @@
-import parse from 'html-react-parser';
-
-import('react-dom').then(function (ReactDOM) {
-  ReactDOM.render(
-    parse('<h1>HTMLReactParser loaded with webpack</h1>'),
-    document.getElementById('root')
-  );
-});
